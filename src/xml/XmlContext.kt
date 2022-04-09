0134ce110e0b80522ef21c2deebea3775059a58b
TariqYoussef <tarek332000@proton.me>
2022-04-09 08:41:26 +0100

    annotations implemented

--- src/xml/XmlContext.kt
+++ src/xml/XmlContext.kt
@@ -14,45 +14,19 @@ class XmlContext(version: String = "1.0", encoding: String = "UTF-8", standalone
     inline fun <reified T : Any>addXmlElement(element: T) {
         val kClass: KClass<T> = T::class
 
-        val elementName: String = if(kClass.hasAnnotation<XmlElementName>())
-        {
-            if(kClass.findAnnotation<XmlElementName>()?.name == null)
-                throw InvalidXmlAnnotationException("XmlElementName")
+        val xmlElement = createXmlElement(kClass, element)
 
-            kClass.findAnnotation<XmlElementName>()?.name!!
-        } else {
-            if(kClass.simpleName == null)
-                throw InvalidXmlElementException("Class doesn't have a name")
-
-            kClass.simpleName!!
-        }
-
-        val elementContent = kClass.declaredMemberProperties.filter{it.hasAnnotation<XmlElementContent>()}
-        if(elementContent.size > 1)
-            throw InvalidXmlAnnotationException("XmlElementContent")
-
-        val xmlElement: XmlElement = if(elementContent[0].call(element) == null)
-        {
-            XmlElement(elementName)
-        }
-        else
-        {
-            XmlElement(elementName, elementContent[0].call(element)!!)
-        }
-
-        kClass.declaredMemberProperties.forEach{
-            if(!it.hasAnnotation<XmlIgnore>() && !it.hasAnnotation<XmlElementContent>())
+        val properties = kClass.declaredMemberProperties.filter{!it.hasAnnotation<XmlElementContent>() && !it.hasAnnotation<XmlIgnore>()}
+        properties.forEach{
+            if(it.call(element) != null)
             {
-                if(it.call(element) != null)
-                {
-                    val propertyXmlElement = XmlElement(it.name, it.call(element)!!)
-                    xmlElement.addChild(propertyXmlElement)
-                }
-                else
-                {
-                    val propertyXmlElement = XmlElement(it.name)
-                    xmlElement.addChild(propertyXmlElement)
-                }
+                val propertyXmlElement = XmlElement(it.name, it.call(element)!!)
+                xmlElement.addChild(propertyXmlElement)
+            }
+            else
+            {
+                val propertyXmlElement = XmlElement(it.name)
+                xmlElement.addChild(propertyXmlElement)
             }
         }
 
@@ -72,5 +46,32 @@ class XmlContext(version: String = "1.0", encoding: String = "UTF-8", standalone
             content += element.dump(intent)
 
         return content
+    }
+
+    fun <T : Any>createXmlElement(kClass: KClass<T>, element: T): XmlElement
+    {
+        val elementName: String = if(kClass.hasAnnotation<XmlElementName>())
+        {
+            if(kClass.findAnnotation<XmlElementName>()?.name == null)
+                throw InvalidXmlAnnotationException("XmlElementName", "Invalid name")
+
+            kClass.findAnnotation<XmlElementName>()?.name!!
+        } else {
+            if(kClass.simpleName == null)
+                throw InvalidXmlElementException("Class doesn't have a name")
+
+            kClass.simpleName!!
+        }
+
+        val elementContent = kClass.declaredMemberProperties.filter{it.hasAnnotation<XmlElementContent>() }
+
+        return if(elementContent.isNotEmpty()) {
+            if(elementContent.size > 1)
+                throw InvalidXmlAnnotationException("XmlElementContent", "Can't be used more than one time in one class")
+
+            XmlElement(elementName, elementContent[0].call(element)!!)
+        } else {
+            XmlElement(elementName)
+        }
     }
 }
