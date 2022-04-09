d2204278e28a51d5bcf83bb9df1c7bb91d6ceb1c
TariqYoussef <tarek332000@proton.me>
2022-04-09 07:53:03 +0100

    Exception type added

--- src/xml/XmlException.kt
+++ src/xml/XmlException.kt
@@ -1,4 +1,4 @@
 package xml
 
 class InvalidXmlElementException(message: String): Exception(message)
-class InvalidXmlAnnotationException(message: String): Exception(message)
+class InvalidXmlAnnotationException(annotationName: String): Exception("Annotation invalid: $annotationName")
