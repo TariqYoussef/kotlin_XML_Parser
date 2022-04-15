301326b5683620526ca3fd8bd2ff1c2f741fbecd
TariqYoussef <tarek332000@proton.me>
2022-04-15 14:59:01 +0100

    type utils update

--- src/main/java/xml/utils/TypeUtils.kt
+++ src/main/java/xml/utils/TypeUtils.kt
@@ -1,7 +1,6 @@
 package xml.utils
 
-fun isBasicType(element: Any): Boolean = (element is Int || element is Short || element is Byte || element is Long
-        || element is Double || element is Float
+fun isBasicType(element: Any): Boolean = (element is Number
         || element is UInt || element is UShort || element is UByte || element is ULong
         || element is Boolean || element is String)
 
