f1b112f153da3ba7102b0de4e11d57a313cd95ae
TariqYoussef <tarek332000@proton.me>
2022-04-15 15:10:32 +0100

    type utils update

--- src/main/java/xml/utils/TypeUtils.kt
+++ src/main/java/xml/utils/TypeUtils.kt
@@ -5,3 +5,8 @@ fun isBasicType(element: Any): Boolean = (element is Number
         || element is Boolean || element is String)
 
 fun isEnum(element: Any): Boolean = element is Enum<*>
+
+fun isArray(element: Any): Boolean = element is Array<*> || element is ByteArray ||
+        element is ShortArray || element is IntArray || element is LongArray
+
+fun isCollection(element: Any): Boolean = element is Collection<*>
