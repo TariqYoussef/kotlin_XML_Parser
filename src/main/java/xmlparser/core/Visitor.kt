4599f445f214e0e6a8591af099611d00ba5dd44f
TariqYoussef <tarek332000@proton.me>
2022-05-01 00:09:50 +0100

    doc added

--- src/main/java/xmlparser/core/Visitor.kt
+++ src/main/java/xmlparser/core/Visitor.kt
@@ -3,8 +3,20 @@ package xmlparser.core
 import xmlparser.core.element.XmlElement
 
 interface Visitor {
+    /**
+     * Visit.
+     */
     fun visit(xmlContext: XmlContext): Boolean = true
+    /**
+     * Visit.
+     */
     fun visit(xmlElement: XmlElement): Boolean = true
+    /**
+     * End Visit.
+     */
     fun endVisit(xmlContext: XmlContext){}
+    /**
+     * End Visit.
+     */
     fun endVisit(xmlElement: XmlElement){}
 }
