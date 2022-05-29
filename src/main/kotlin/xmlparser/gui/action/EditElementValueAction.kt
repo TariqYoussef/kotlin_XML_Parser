c87d246292441a14d27f5e156badfd15e19af2c3
TariqYoussef <tarek332000@proton.me>
2022-05-29 07:40:34 +0100

    updates

--- src/main/kotlin/xmlparser/gui/action/EditElementValueAction.kt
+++ src/main/kotlin/xmlparser/gui/action/EditElementValueAction.kt
@@ -4,9 +4,10 @@ import xmlparser.core.element.XmlElement
 import xmlparser.gui.IAction
 
 class EditElementValueAction(private val xmlElement: XmlElement, private val newValue: String) : IAction {
-    override val name: String = ""
 
     private val oldValue: String = xmlElement.value as String
+
+    override val name: String = "Edit Element Value to $newValue from $oldValue"
 
     override fun execute() {
         xmlElement.value = newValue
---
+++ src/main/kotlin/xmlparser/gui/action/RemoveAttributeAction.kt
@@ -0,0 +1,16 @@
+package xmlparser.gui.action
+
+import xmlparser.core.element.XmlElement
+import xmlparser.core.element.XmlElementAttribute
+import xmlparser.gui.IAction
+
+class RemoveAttributeAction(private val xmlElement: XmlElement, private val xmlElementAttribute: XmlElementAttribute) : IAction {
+    override val name: String = "Remove Attribute ${xmlElementAttribute.name} from ${xmlElement.name}"
+    override fun execute() {
+        xmlElement.removeAttribute(xmlElementAttribute)
+    }
+
+    override fun undo() {
+        xmlElement.addAttribute(xmlElementAttribute)
+    }
+}
