e465bd422f97ad49fe494872bd5ca16664bf49bc
TariqYoussef <tarek332000@proton.me>
2022-05-29 02:34:41 +0100

    refactoring

--- src/main/kotlin/xmlparser/gui/IAction.kt
+++ src/main/kotlin/xmlparser/gui/IAction.kt
@@ -2,6 +2,14 @@ package xmlparser.gui
 
 interface IAction {
     val name: String
+
+    /**
+     * Executes action.
+     */
     fun execute()
+
+    /**
+     * Undo the action
+     */
     fun undo()
 }
