ed51408a13489051ae199122fdba0f7c8560797c
TariqYoussef <tarek332000@proton.me>
2022-05-29 02:42:21 +0100

    main not needed

--- src/main/kotlin/xmlparser/gui/Application.kt
+++ src/main/kotlin/xmlparser/gui/Application.kt
@@ -1,6 +1,11 @@
 package xmlparser.gui
 
 import tornadofx.App
+import tornadofx.launch
 import xmlparser.gui.views.MainView
 
 class Application : App(MainView::class)
+
+fun main(args: Array<String>) {
+    launch<Application>(args)
+}
--- src/main/kotlin/xmlparser/gui/main.kt
+++
@@ -1,7 +0,0 @@
-package xmlparser.gui
-
-import tornadofx.launch
-
-fun main(args: Array<String>) {
-    launch<Application>(args)
-}
