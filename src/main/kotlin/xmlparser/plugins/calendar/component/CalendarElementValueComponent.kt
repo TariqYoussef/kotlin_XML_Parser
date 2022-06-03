b02cdbd105c4e60b967b754f96c5020ee5e74032
TariqYoussef <tarek332000@proton.me>
2022-06-03 01:50:25 +0100

    refactoring

--- src/main/kotlin/xmlparser/plugins/calendar/component/CalendarElementValueComponent.kt
+++ src/main/kotlin/xmlparser/plugins/calendar/component/CalendarElementValueComponent.kt
@@ -5,6 +5,9 @@ import xmlparser.gui.view.component.IComponent
 import javax.swing.JPanel
 
 class CalendarElementValueComponent : IComponent<ElementView> {
+
+    override fun accept(view: ElementView): Boolean = view.xmlElement.name == "Calendar"
+
     override fun component(view: ElementView): JPanel? {
         return null
     }
